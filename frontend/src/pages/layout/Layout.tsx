312a242bcb67f3305281860d12d520997b2c8710
Johan <97096082+JohanOgren@users.noreply.github.com>
2024-02-22 08:30:38 +0100

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -47,7 +47,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
                     <Stack horizontal verticalAlign="center">
-                        <img src="frontend/src/assets/Contoso.svg" alt="Contoso Logo" id="logo">
+                        <img src=".../src/assets/Contoso.svg" alt="Contoso Logo" id="logo">
                         <Link to="/" className={styles.headerTitleContainer}>
                             <h1 className={styles.headerTitle}>{ui?.title}</h1>
                         </Link>
